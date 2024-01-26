65e704f53d6f5f8c459e55583d4b195e0979a94c
chinmay0910 <chinmayrmhatre@gmail.com>
2024-01-27 02:01:46 +0530

    Update MongoDB Database

--- src/db/conn.js
+++ src/db/conn.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+require('dotenv').config()
 
-mongoose.connect("mongodb+srv://chinmayrmhatre:97M9OZOJRJDOh1kM@expressdelivery.zr6wdfo.mongodb.net/expressDelivery?retryWrites=true&w=majority").then(console.log("Connected to server"))
+mongoose.connect(process.env.MONGODB_URL).then(console.log("Connected to server"))
 .catch((e)=> console.log(`Not Connected -- ${e}`));
