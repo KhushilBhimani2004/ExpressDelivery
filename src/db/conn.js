06170ab11755345742c9ba0ad39fc2e87f771725
chinmay0910 <chinmayrmhatre@gmail.com>
2024-01-27 01:56:16 +0530

    Update MongoDB Database

--- src/db/conn.js
+++ src/db/conn.js
@@ -1,4 +1,4 @@
 const mongoose = require('mongoose');
 
-mongoose.connect("mongodb://0.0.0.0:27017/expressDelivery").then(console.log("Connected to server"))
+mongoose.connect("mongodb+srv://chinmayrmhatre:97M9OZOJRJDOh1kM@expressdelivery.zr6wdfo.mongodb.net/expressDelivery?retryWrites=true&w=majority").then(console.log("Connected to server"))
 .catch((e)=> console.log(`Not Connected -- ${e}`));
